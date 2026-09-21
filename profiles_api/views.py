19bacfa64635af18e0738e1acf620b542c137e07
mmdparimoon <pythonparimoon@gmail.com>
2026-09-21 21:25:10 +0330

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,viewsets
 from .serializers import HelloSerializer
 
 class HelloApiView(APIView):
@@ -45,3 +45,52 @@ class HelloApiView(APIView):
     def delete(self,request,pk=None):
         """Delete an object"""
         return Response({'method:"DELETE'})    
+  
+  
+class HelloViewSets(viewsets.ViewSet):
+    """Test API VieweSet""" 
+    serializer_class=HelloSerializer
+    def list(self,request):
+        """Return a hello message"""
+        
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+        
+        return Response({'message':'Hello!','a_viewset':a_viewset})
+    
+    def create(self,request):
+        """Create a new hello message"""
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}!'
+
+            return Response({"message":message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            ) 
+            
+    def retrieve(self,request,pk=None):
+        """Handle getting  an object  by its ID"""
+        return Response({'methods':'GET'})            
+
+
+    def update(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'methods':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        """Handle partial updating an object"""
+        return Response({'methods':'PATCH'})
+    
+    
+    
+    def destroy(self,request,pk=None):
+        """Handel removing an object"""
+        
+        return Response({'methods':'DELETE'})
